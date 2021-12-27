95bdc7ddd3e9fa5bba68a80473885d9d5949ffca
Giuseppe De Palma <dpl.gsp@tutanota.com>
2021-12-27 19:13:33 +0100

    feat: add task options definitions

--- nuv/commands/task.go
+++ nuv/commands/task.go
@@ -26,6 +26,23 @@ import (
 )
 
 type TaskCmd struct {
+	Color       bool   `help:"colored output. Enabled by default. Set flag to false or use NO_COLOR=1 to disable (default true)" default:"true" short:"c" env:"NO_COLOR"`
+	Concurrency int    `help:"limit number tasks to run concurrently" short:"C"`
+	Dir         string `help:"sets directory of execution" short:"d"`
+	Dry         bool   `help:"compiles and prints tasks in the order that they would be run, without executing them"`
+	Force       bool   `help:"forces execution even when the task is up-to-date" short:"f"`
+	Help        bool   `help:"shows Task usage" short:"h"`
+	Init        bool   `help:"creates a new Taskfile.yml in the current folder" short:"i"`
+	List        bool   `help:"lists tasks with description of current Taskfile" short:"l"`
+	Output      string `help:"sets output style: [interleaved|group|prefixed]" short:"o"`
+	Parallel    bool   `help:"executes tasks provided on command line in parallel" short:"p"`
+	Silent      bool   `help:"disables echoing" short:"s"`
+	Status      bool   `help:"exits with non-zero exit code if any of the given tasks is not up-to-date"`
+	Summary     bool   `help:"show summary about a task"`
+	Taskfile    string `help:"choose which Taskfile to run. Defaults to Taskfile.yml"`
+	Verbose     bool   `help:"enables verbose mode" short:"v"`
+	Version     bool   `help:"show Task version"`
+	Watch       bool   `help:"enables watch of the given task" short:"w"`
 }
 
 func (t *TaskCmd) Run() error {
