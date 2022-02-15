a51af4fb5141fda5d4a747d0d8251d211660f476
Davide Cocco <Davide_Cocco@epitmilw0030.lan>
2022-02-15 17:28:33 +0100

    Fix wsk_apihost

--- nuv/wsk_properties.go
+++ nuv/wsk_properties.go
@@ -24,7 +24,7 @@ import (
 )
 
 const wsk_auth = "23bc46b1-71f6-4ed5-8c54-816aa4f8c502:123zO3xZCLrMN6v2BKK1dXYFpXlPkccOFqm12CdAsMgRU4VrNZ9lyGVCGuMDGIwP"
-const wsk_apihost = "https://localhost:3233"
+const wsk_apihost = "http://localhost:3233"
 
 func writeWskPropertiesFile() error {
 	homedir, err := GetHomeDir()
