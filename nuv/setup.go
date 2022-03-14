e3fda68667274bc548d9fdb72be14d5fe1b74a3e
Giuseppe De Palma <dpl.gsp@tutanota.com>
2022-03-14 19:35:39 +0100

    Small setupNuvolaris refactor (#35)
    * Add xor group for devcluster and reset flag

--- nuv/setup.go
+++ nuv/setup.go
@@ -18,9 +18,9 @@
 package main
 
 type SetupCmd struct {
-	Devcluster bool   `help:"start dev kind k8s cluster"`
+	Devcluster bool   `help:"start dev kind k8s cluster" xor:"dev-or-reset"`
 	ImageTag   string `default:"neo-22.0207.21" help:"nuvolaris operator docker image tag to deploy"`
-	Reset      bool   `help:"reset nuvolaris setup"`
+	Reset      bool   `help:"reset nuvolaris setup" xor:"dev-or-reset"`
 }
 
 func (setupCmd *SetupCmd) Run() error {
