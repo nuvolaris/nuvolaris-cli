2e42f526abe1749258c777f892d33c5448eaff22
Michele Sciabarra <michele@sciabarra.com>
2022-03-18 09:19:18 +0000

    fixed version, updated taskfile to use other clusters

--- nuv/main.go
+++ nuv/main.go
@@ -23,11 +23,11 @@ import (
 
 // CLIVersion holds the current version, to be set by the build with
 //  go build -ldflags "-X main.CLIVersion=<version>"
-const CLIVersion string = "latest"
+var CLIVersion string = "latest"
 
 // ImageTag holds the version of the Docker image used for the nuvolaris
 // operator used in setup
-const ImageTag string = "neo-22.0207.21"
+var ImageTag string = "0.2.0-trinity.22031805"
 
 type CLI struct {
 	Deploy     DeployCmd     `cmd:"" help:"deploy a nuvolaris cluster"`
